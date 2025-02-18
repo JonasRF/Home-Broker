fe31c06cd9b6487ce494e4ca07cfd29067200991
root <root@PC-JONAS>
2025-02-18 00:21:06 -0300

    Assets service, controller and conection with database mongodb

--- src/assets/entities/asset.entity.ts
+++ src/assets/entities/asset.entity.ts
@@ -1,1 +1,27 @@
-export class Asset {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as crypto from 'crypto';
+import { HydratedArraySubdocument } from 'mongoose';
+
+export type AssetDocument = HydratedArraySubdocument<Asset>;
+
+@Schema({ timestamps: true })
+export class Asset {
+  @Prop({ default: () => crypto.randomUUID() })
+  _id: string;
+
+  @Prop({ unique: true, index: true })
+  name: string;
+
+  @Prop({ unique: true, index: true })
+  symbol: string;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  price: number;
+
+  createdAt!: Date;
+  updatedAt!: Date;
+}
+export const AssetSchema = SchemaFactory.createForClass(Asset);
