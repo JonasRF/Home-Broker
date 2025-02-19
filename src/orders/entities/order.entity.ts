506fbaef19bf1b81b5895975d5305a1c419439db
JonasRF <jonasribeiro41@yahoo.com.br>
2025-02-19 16:21:08 -0300

    Creteded Order

---
+++ src/orders/entities/order.entity.ts
@@ -0,0 +1,45 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedArraySubdocument } from 'mongoose';
+import * as crypto from 'crypto';
+import { Asset, AssetDocument } from 'src/assets/entities/asset.entity';
+import { Wallet, WalletDocument } from 'src/wallets/entities/wallet.entity';
+
+export type OrderDocument = HydratedArraySubdocument<Order>;
+
+export enum OrderType {
+  BUY = 'BUY',
+  SELL = 'SELL',
+}
+
+export enum OrderStatus {
+  PENDING = 'PENDING',
+  OPEN = 'OPEN',
+  CLOSED = 'CLOSED',
+  FAILED = 'FAILED',
+}
+
+@Schema({ timestamps: true })
+export class Order {
+  @Prop({ default: () => crypto.randomUUID() })
+  _id: string;
+
+  @Prop({ type: mongoose.Schema.Types.Int32 })
+  shares: number;
+
+  @Prop({ type: mongoose.Schema.Types.Int32 })
+  partial: number;
+
+  @Prop()
+  price: number;
+
+  @Prop({ type: String, ref: Wallet.name })
+  wallet: WalletDocument | string;
+
+  @Prop({ type: String, ref: Asset.name })
+  asset: AssetDocument | string;
+
+  createdAt!: Date;
+  updatedAt!: Date;
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
