dfd685f4c51b5caee25c2d7c6a23c2dc5fef37b7
JonasRF <jonasribeiro41@yahoo.com.br>
2025-02-21 15:29:50 -0300

    Refactoring walletService and created Dockerfile for replicaset

--- src/wallets/entities/wallet.entity.ts
+++ src/wallets/entities/wallet.entity.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedArraySubdocument } from 'mongoose';
+import mongoose, { HydratedArraySubdocument } from 'mongoose';
 import * as crypto from 'crypto';
+import { WalletAsset, WalletAssetDocument } from './wallet-asset.entity';
 
 export type WalletDocument = HydratedArraySubdocument<Wallet>;
 
@@ -8,6 +9,13 @@ export type WalletDocument = HydratedArraySubdocument<Wallet>;
 export class Wallet {
   @Prop({ default: () => crypto.randomUUID() })
   _id: string;
+
+  @Prop({
+    type: [mongoose.Schema.Types.String],
+    set: (v) => [...new Set(v)],
+    ref: WalletAsset.name,
+  })
+  assets: WalletAssetDocument[] | string[];
 
   createdAt!: Date;
   updatedAt!: Date;
